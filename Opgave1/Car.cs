029c0965eb65804dc8f139ed46de65e4b05efc03
LailaDM <lail0894@edu.easj.dk>
2022-03-07 17:21:23 +0100

    Add project files.

---
+++ Opgave1/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1
+{
+    public class Car
+    {
+        public int _Id;
+        public string _Model;
+        public int _Price;
+        public string _LicensePlate; 
+
+        public Car(int id, string model, int price, string licensePlate)
+        {
+            Id = id;
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+
+        public int Id
+        {
+            get => _Id;
+            set => _Id = value;
+        }
+
+        public string Model
+        {
+            get => _Model;
+            set
+            {
+                if (value.Length < 4) { throw new ArgumentException(); }
+                _Model = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _Price;
+            set
+            {
+                if (value < 0) { throw new ArgumentOutOfRangeException(); }
+                _Price = value;
+            }
+        }
+
+        public string LicensePlate
+        {
+            get => _LicensePlate;
+            set
+            {
+                if (2 <= value.Length && value.Length <= 7) _LicensePlate = value;
+                else throw new ArgumentOutOfRangeException();
+
+            }
+
+        }
+    }
+}
---
+++ Opgave1Tests/CarTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car _car;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _car = new Car(10,"Audi", 1000000, "AB12345");
+        }
+
+
+
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Car c = new Car(10, "Audi", 1000000, "AB12345");
+
+            Assert.AreEqual("Audi", c.Model);
+            Assert.AreEqual(1000000, c.Price);
+            Assert.AreEqual("AB12345", c.LicensePlate);
+        }
+
+        [TestMethod]
+        public void TestModel()
+        {
+            _car.Model = "Audi";
+            Assert.AreEqual("Audi", _car.Model);
+            Assert.ThrowsException<ArgumentException>(() => _car.Model = "ab");
+        }
+
+        [TestMethod]
+        public void TestPrice()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.Price = -1);
+            _car.Price = 1000000;
+            Assert.AreEqual(1000000, _car.Price);
+        }
+
+        [TestMethod]
+            public void TestLicensePlate()
+            {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.LicensePlate = "a");
+                _car.LicensePlate = "AB12345";
+                Assert.AreEqual("AB12345", _car.LicensePlate);
+            }
+
+            
+        }
+    }
